3d22755b4b451545aefba065148cb8fe9b6ed395
hugo-ferguson <47346674+hugo-ferguson@users.noreply.github.com>
2023-09-07 12:29:33 +1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ import { RGB } from './modules/RGB.js';
 
 // Default values for operation and input colours.
 let operation = "add";
-let rgbColour1 = new RGB(0, 0, 0);
-let rgbColour2 = new RGB(0, 0, 0);
+let rgb1 = new RGB(0, 0, 0);
+let rgb2 = new RGB(0, 0, 0);
 
 /**
  * Calculate the selected operation for the two
@@ -17,35 +17,35 @@ function calculate() {
 
     // Update the two RGB colours to the selected values
     // from the colour pickers.
-    rgbColour1.fromString(pickerColour1.value);
-    rgbColour2.fromString(pickerColour2.value);
+    rgb1.fromString(pickerColour1.value);
+    rgb2.fromString(pickerColour2.value);
 
     // Create a Duffy representation of the two RGB colours.
-    let duffyColour1 = Transform.RGBToDuffy(rgbColour1);
-    let duffyColour2 = Transform.RGBToDuffy(rgbColour2);
-    let duffyColour3;
+    let d1 = Transform.RGBToDuffy(rgb1);
+    let d2 = Transform.RGBToDuffy(rgb2);
+    let d3;
 
     // Perform the corresponding operation in DuffySpace for the
     // selected operation.
     switch(operation) {
         case "add":
-            duffyColour3 = DuffySpace.add(duffyColour1, duffyColour2);
+            d3 = DuffySpace.add(d1, d2);
             break;
         case "mul":
-            duffyColour3 = DuffySpace.mul(duffyColour1, duffyColour2);
+            d3 = DuffySpace.mul(d1, d2);
             break;
         case "div":
-            duffyColour3 = DuffySpace.div(duffyColour1, duffyColour2);
+            d3 = DuffySpace.div(d1, d2);
             break;
         case "sub":
-            duffyColour3 = DuffySpace.sub(duffyColour1, duffyColour2);
+            d3 = DuffySpace.sub(d1, d2);
             break;
 
         
     }
 
     // Update the output colour picker value.
-    pickerColour3.value = Transform.DuffyToRGB(duffyColour3).toString();
+    pickerColour3.value = Transform.DuffyToRGB(d3).toString();
 }
 
 // Element references.
